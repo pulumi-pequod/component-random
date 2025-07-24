0244acd27d78deb38d1031eafdb6fb85e08338b9
MitchellGerdisch <mitch@pulumi.com>
2025-07-24 08:19:57 -0500

    initial commit

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { PetAbstracted } from "./petAbstracted";
---
+++ petAbstracted.ts
@@ -0,0 +1,48 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
+
+export interface PetAbstractedArgs {
+    size: string;
+}
+
+// This resource abstracts using the RandomPet resource to generate a pet name based on a size parameter.
+export class PetAbstracted extends pulumi.ComponentResource {
+    // Return the generated pet name
+    public readonly petName: pulumi.Output<string>;
+
+    constructor(name: string, args: PetAbstractedArgs, opts?: pulumi.ComponentResourceOptions) {
+        super("random-abstracted:index:PetAbstracted", name, args, opts);
+
+        const size = args.size || "small";
+
+        var petNameLength: number;
+        switch (size) {
+            case "small":
+                petNameLength = 2;
+                break;
+            case "medium":
+                petNameLength = 4;
+                break;
+            case "large":
+                petNameLength = 6;
+                break;
+            case "xlarge":
+                petNameLength = 8;
+                break;
+            default:
+                petNameLength = Number(size);
+                if (isNaN(petNameLength)) {
+                    throw new Error(`Invalid size: "${size}". Must be "small", "medium", "large" or "xlarge" or a number.`);
+                }
+        }
+
+        const petName = new random.RandomPet("petName", {
+            length: petNameLength,
+        }, { parent: this });
+
+        this.petName = petName.id
+
+        this.registerOutputs({});
+
+    }
+}
