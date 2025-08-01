4554ed7b6cfb149bb4cb28c733517f9572eace2f
MitchellGerdisch <mitch@pulumi.com>
2025-08-01 14:04:00 -0500

    debugging

--- .github/workflows/update-policy-config.py
+++ .github/workflows/update-policy-config.py
@@ -140,7 +140,12 @@ def update_policy_config():
                     
                     # PATCH request to update the policy
                     print(f"Updating policy group at: {api_url}")
-                    headers['Authorization'] = f'token {os.environ.get('TEMP_TOKEN')}'
+                    temp_token = os.environ.get('TEMP_TOKEN')
+                    if temp_token:
+                        headers['Authorization'] = f'token {temp_token}'
+                    else:
+                        print("No TEMP_TOKEN found, using default auth token")
+
                     print(f"PATCH body: {json.dumps(patch_body, indent=2)}")
                     
                     patch_response = requests.patch(
